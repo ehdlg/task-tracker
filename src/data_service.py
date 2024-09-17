535345b2662c4190d9e52068e08eb67cacad914e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-17 11:44:10 +0200

    feat(service): add data service to handle getting and updating json data

---
+++ src/data_service.py
@@ -0,0 +1,25 @@
+import json
+
+DEFAULT_FILE_VALUE: list = []
+
+
+def get_tasks() -> list:
+    try:
+        with open("../data.json", "r") as tasks_file:
+            raw_data = tasks_file.read()
+            data = json.loads(raw_data)
+
+            return data
+    except Exception:
+        persist_data(DEFAULT_FILE_VALUE)
+
+        return DEFAULT_FILE_VALUE
+
+
+def update_tasks(new_tasks: list) -> None:
+    persist_data(new_tasks)
+
+
+def persist_data(new_data: list) -> None:
+    with open("../data.json", "w+") as f:
+        json.dump(new_data, f)
