f147c743a35b737efb0e1bd2c57de0c2230dce30
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-18 06:05:12 +0200

    feat: implement actions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
 from parser import parser
 
+from actions import (
+    add_task,
+    delete_task,
+    mark_done,
+    mark_in_progress,
+    mark_todo,
+    update,
+)
+
 args = parser.parse_args()
+
+ACTIONS = {
+    "add": lambda args: add_task(args.description),
+    "delete": lambda args: delete_task(args.id),
+    "update": lambda args: update(args.id, description=args.description),
+    "mark-in-progress": lambda args: mark_in_progress(args.id),
+    "mark-done": lambda args: mark_done(args.id),
+    "mark-todo": lambda args: mark_todo(args.id),
+}
 
 
 if args.command is None:
@@ -8,5 +26,4 @@ if args.command is None:
 
     exit()
 
-
-exit()
+ACTIONS[args.command](args)
